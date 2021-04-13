db673335030704a33a42d1318f4cfc38c157f085
Rachel-Reidenga <reidenga@gmail.com>
2021-04-12 22:42:15 -0500

    auth

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const seedPosts = require('./post-seeds');
 const seedUsers = require('./user-seeds');
 const seedComments = require('./comment-seeds');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
